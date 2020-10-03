7efd7be77a183b4061827c60791a83f7873f035c
hassan11196 <hassan11196@hotmail.com>
2020-10-03 15:41:03 +0500

    refactor x,y to Position

--- src/main/java/Game.java
+++ src/main/java/Game.java
@@ -2,33 +2,48 @@ import exceptions.InvalidFirstPlayerException;
 import exceptions.InvalidPositionException;
 import exceptions.ConsecutivePlayException;
 
+import java.security.PrivateKey;
+
 public class Game {
     private char _lastSymbol = ' ';
     private Board _board = new Board();
 
     public void Play(char symbol, int x, int y) throws Exception {
-        checkForInvalidMove(symbol, x, y);
+        Position position = new Position(x, y);
+        checkForInvalidMove(symbol, position);
 
         // update game state
         _lastSymbol = symbol;
         _board.AddTileAt(symbol, x, y);
     }
 
-    private void checkForInvalidMove(char symbol, int x, int y) throws Exception {
+    private void checkForInvalidMove(char symbol, Position position) throws Exception {
         //if first move
+        firstPlayerIsO(symbol);
+        //if not first move but player repeated
+        repeatedMove(symbol);
+        //if not first move but play on an already played tile
+        invalidPosition(position);
+    }
+
+    private void invalidPosition(Position position) throws InvalidPositionException {
+        if (_board.TileAt(position.row, position.col).Symbol != ' ') {
+            throw new InvalidPositionException();
+        }
+    }
+
+    private void repeatedMove(char symbol) throws ConsecutivePlayException {
+        if (symbol == _lastSymbol) {
+            throw new ConsecutivePlayException();
+        }
+    }
+
+    private void firstPlayerIsO(char symbol) throws InvalidFirstPlayerException {
         if (_lastSymbol == ' ') {
             //if player is X
             if (symbol == 'O') {
                 throw new InvalidFirstPlayerException();
             }
-        }
-        //if not first move but player repeated
-        else if (symbol == _lastSymbol) {
-            throw new ConsecutivePlayException();
-        }
-        //if not first move but play on an already played tile
-        else if (_board.TileAt(x, y).Symbol != ' ') {
-            throw new InvalidPositionException();
         }
     }
 
